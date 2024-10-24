9c3a00490cc3af635b9ddfaa8bb382b36530d6bd
Suraj Shanbhag <shanbhagsuraj84@gmail.com>
2024-10-24 14:44:32 +0530

    Update config.php

--- config/config.php
+++ config/config.php
@@ -16,11 +16,6 @@
     $conn = new PDO("mysql:host=$host;dbname=$dbname;", $user. $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    if($conn == true) {
-        echo "Connection Established";
-    } else {
-        echo "Connection is not established";
-    }
 
 
 ?>
